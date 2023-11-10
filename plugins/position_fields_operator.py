eaf6d4b722bdfb3e1a14cca0de9aeac68c7daa33
Jamesson Faria <jamessonjr@gmail.com>
2023-11-10 10:04:01 -0300

    adjust in plugin

--- plugins/position_fields_operator.py
+++ plugins/position_fields_operator.py
@@ -15,22 +15,15 @@ class PositionFieldsOperator(BaseOperator):
         self.position_fields = position_fields
 
     def get_fields(self, lin):
-        fields = []
-        start = 0
-        for start, end in self.position_fields:
-            field = lin[start:end].strip()
-            fields.append(field)
-        return fields
-    
-
+       return [lin[start:end].strip() for start, end in self.position_fields]
+        
     def transform_data(self):
         df = pd.read_csv(self.path_input_file, header=None)
 
         df['fields'] = df[0].apply(self.get_fields)
         df['row'] = df.index + 1
-        df = df[['row', 'fields']]
 
-        data_dict = df.to_dict(orient='records')
+        data_dict = df[['row', 'fields']].to_dict(orient='records')
 
         with open(self.path_output_file, 'w') as json_file:
             json.dump(data_dict, json_file, indent=4)
